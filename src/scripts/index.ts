33cf9b968278307592e2520643b2f305bf629d23
Remi Planel <rplanel@pasteur.fr>
2019-04-01 18:05:22 +0200

    add the main

---
+++ src/scripts/index.ts
@@ -0,0 +1,14 @@
+import GeneComponent from "./component/sequence/gene";
+import { select, Selection } from "d3-selection";
+const genes = [
+    { name: "gene 1", length: 1000, strand: 1 },
+    { name: "gene 2", length: 500, strand: -1 }
+]
+
+const geneComponent = GeneComponent();
+
+
+const myGenes = select("g#genes");
+
+myGenes.datum(genes).call(geneComponent, 1000, 5, 10);
+
