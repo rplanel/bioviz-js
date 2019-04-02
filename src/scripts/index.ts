5b5adf360afc0d2edb42c461f3e7950dd4d96910
Remi Planel <rplanel@pasteur.fr>
2019-04-02 17:06:15 +0200

    Add small gene

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,7 +2,11 @@ import GeneComponent, { GeneData } from "./component/sequence/gene";
 import { select, Selection } from "d3-selection";
 const genes = [
     { name: "gene 1", length: 1000, strand: 1 },
-    { name: "gene 2", length: 500, strand: -1 }
+    { name: "gene 2", length: 500, strand: -1 },
+    { name: "gene 3", length: 500, strand: 1 },
+    { name: "gene 4", length: 20, strand: 1 },
+    { name: "gene 3", length: 250, strand: -1 },
+
 ]
 
 const geneComponent = GeneComponent();
@@ -10,5 +14,5 @@ const geneComponent = GeneComponent();
 
 const myGenes = select<SVGGElement, Array<GeneData>>("g#genes");
 
-myGenes.datum(genes).call(geneComponent, 1000, 5, 10);
+myGenes.datum(genes).call(geneComponent, 1000, 10, 50);
 
