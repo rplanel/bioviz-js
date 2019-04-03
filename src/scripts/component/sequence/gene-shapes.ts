b78b1eff57b328a9d5eb33d1919558258ff7c26b
Remi Planel <rplanel@pasteur.fr>
2019-04-03 11:35:30 +0200

    By default draw gene as "+" strand.

--- src/scripts/component/sequence/gene-shapes.ts
+++ src/scripts/component/sequence/gene-shapes.ts
@@ -8,17 +8,17 @@ export function arrowShape(
     const { position: { x, y, width } } = data;
     const genePath = path();
     if (width < arrowWidth) {
-        genePath.moveTo(0, geneHeight / 2);
-        genePath.lineTo(width, geneHeight);
-        genePath.lineTo(width, 0);
+        genePath.moveTo(0, 0)
+        genePath.moveTo(0, geneHeight)
+        genePath.moveTo(width, geneHeight / 2)
         genePath.closePath();
     }
     else {
-        genePath.moveTo(0, geneHeight / 2);
-        genePath.lineTo(arrowWidth, geneHeight);
-        genePath.lineTo(width, geneHeight);
-        genePath.lineTo(width, 0);
-        genePath.lineTo(arrowWidth, 0);
+        genePath.moveTo(0, 0);
+        genePath.lineTo(0, geneHeight);
+        genePath.lineTo(width - arrowWidth, geneHeight);
+        genePath.lineTo(width, geneHeight / 2);
+        genePath.lineTo(width - arrowWidth, 0);
         genePath.closePath();
     }
     return genePath.toString();
