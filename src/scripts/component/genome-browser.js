5a4cf27a522f7ea5a1a529f3398e614058bd09c1
Remi Planel <rplanel@pasteur.fr>
2019-04-08 14:45:53 +0200

    rm src/scripts/component/genome-browser.js

--- src/scripts/component/genome-browser.js
+++
@@ -1,9 +0,0 @@
-import GeneComponent, { GeneData } from "./component/sequence/gene";
-import GenomeAxis from "./component/sequence/genome-axis";
-
-export default function () {
-  function genomeBrowser(_selection) { 
-    _selection.each();
-  }
-  return genomeBrowser;
-}
