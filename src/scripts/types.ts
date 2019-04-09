22e07a2d48a9cf10c6dd71146ba58c2d9c8a890b
Remi Planel <rplanel@pasteur.fr>
2019-04-09 16:50:00 +0200

    Add types GenericAxisData, GlobalAxisData, GenomeBrowserData

--- src/scripts/types.ts
+++ src/scripts/types.ts
@@ -19,3 +19,38 @@ export interface PositionedGeneData extends GeneData {
     width: number
   }
 }
+
+
+export interface GenericAxisData {
+  title: string,
+  interval: [number, number]
+}
+
+export interface GlobalAxisData extends GenericAxisData {
+  window: [number, number]
+}
+
+
+
+export interface GenomeBrowserData {
+  width: number,
+  genomeWindow: {
+    center: number,
+    size: number
+  },
+  currentMousePosition: number,
+  chromosome: {
+    size: number,
+    genes: GeneData[]
+  },
+  axis: {
+    global: GlobalAxisData,
+    chromosome: GenericAxisData
+
+  }
+  eventHandler?: {
+    dragstarted: (elem: SVGElement) => void,
+    dragged: () => void,
+    dragended: (elem: SVGElement) => void
+  }
+}
