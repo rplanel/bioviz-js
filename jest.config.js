55baa6ae23a62a66f14096b033e0ff510d877a21
Remi Planel <rplanel@pasteur.fr>
2019-05-29 14:35:37 +0200

    Add test coverage

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,6 @@ module.exports = {
   },
   testRegex: "/tests/.*\\.(test|spec)\\.(jsx?|tsx?|ts)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true,
+  coverageReporters: ["text-summary"]
 };
