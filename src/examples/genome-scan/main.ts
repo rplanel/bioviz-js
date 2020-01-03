500890987c71aee2b878b6be06c5432a9f4d63a4
Remi Planel <rplanel@pasteur.fr>
2020-01-03 19:11:49 +0100

    Add qtl coefficient to example

--- src/examples/genome-scan/main.ts
+++ src/examples/genome-scan/main.ts
@@ -1,15 +1,33 @@
-import { GenomeScanData } from "../../scripts/types";
+import { LodScoreOnChromosome } from "../../scripts/types";
 import { select } from "d3-selection";
 import GenomeScan from "../../scripts/component/genome/genome-scan";
-
+import QtlCoef from "../../scripts/component/genome/chromosome/qtl-coefficient";
 import { csv } from "d3-fetch";
 
 
-const csv_file = require('./lod_tab.csv');
+const genomeScanFile = require('./data/lod_tab.csv');
+const significanceThresholdsFile = require('./data/sifgnificance-thresholds.csv');
+const qtlCoefCsv = require('./data/dat-plotcoef.csv');
 
 // Code
 const data = {};
-csv(csv_file).then(function (genomeScanData) {
+Promise.all([
+  csv(genomeScanFile),
+  csv(significanceThresholdsFile),
+  csv(qtlCoefCsv),
+]).then(([genomeScanData, significanceThresholds, plotCoefData]) => {
   const genomeScan = GenomeScan();
-  select(".genome-scan").datum(genomeScanData).call(genomeScan)
+  const data = {
+    lod_score_on_chromosome: genomeScanData,
+    significance_thresholds: significanceThresholds,
+  };
+
+  const qtlCoefPlot = QtlCoef();
+  select(".lod-score-chromosomes").datum(data).call(genomeScan);
+  select(".qtl-coefficient").datum(plotCoefData).call(qtlCoefPlot);
 })
+
+// csv(genomeScanFile).then(function (genomeScanData) {
+//   const genomeScan = GenomeScan();
+//   select(".genome-scan").datum(genomeScanData).call(genomeScan)
+// })
