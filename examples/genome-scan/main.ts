673684e2a0aec815d54fff681353ffd8f6b0464d
Remi Planel <rplanel@pasteur.fr>
2020-01-07 16:00:23 +0100

    Move example outside lib

--- examples/genome-scan/main.ts
+++ examples/genome-scan/main.ts
@@ -1,13 +1,16 @@
-import { GenomeScanData, HaplotypeData, PlotCoefData, SnpsData, SnpData } from "../../scripts/types";
+// import { GenomeScanData, HaplotypeData, PlotCoefData, SnpsData, SnpData } from "bioviz-js";
 import { select } from "d3-selection";
-import GenomeScan from "../../scripts/component/genome/genome-scan";
-import QtlCoef from "../../scripts/component/genome/chromosome/qtl-coefficient";
-import Haplotype from "../../scripts/component/genome/chromosome/haplotype";
-import Snps from "../../scripts/component/genome/chromosome/snps";
-import Snp from "../../scripts/component/genome/snp/snp";
+
+import { Types, GenomeScan, QtlCoefficient, Haplotype, Snps, Snp } from "bioviz-js";
+
+// import GenomeScan from "../../scripts/component/qtl/genome-scan";
+// import QtlCoef from "../../scripts/component/qtl/qtl-coefficient";
+// import Haplotype from "../../scripts/component/qtl/haplotype";
+// import Snps from "../../scripts/component/qtl/snps";
+// import Snp from "../../scripts/component/qtl/snp";
+
 
 import { csv } from "d3-fetch";
-import { cpus } from "os";
 
 // Csv data file
 const genomeScanFile = require('./data/lod_tab.csv');
@@ -18,7 +21,6 @@ const snpsCsv = require('./data/dat_plot_snps.csv');
 const snpCsv = require("./data/dat_plot_snp_boxplot.csv");
 
 // Code
-const data = {};
 Promise.all([
   csv(genomeScanFile),
   csv(significanceThresholdsFile),
@@ -29,19 +31,19 @@ Promise.all([
 ]).then(([genomeScanData, significanceThresholds, rawPlotCoefData, rawHaplotypeData, rawSnpsData, rawSnpData]) => {
   // Plot component
   const genomeScan = GenomeScan();
-  const qtlCoefPlot = QtlCoef();
+  const qtlCoefPlot = QtlCoefficient();
   const haplotypePlot = Haplotype();
   const snpsPlot = Snps();
   const snpPlot = Snp();
 
   // Data
-  const haplotypeData: HaplotypeData[] = rawHaplotypeData.map(item => ({
+  const haplotypeData: Types.HaplotypeData[] = rawHaplotypeData.map(item => ({
     ...item,
     Phenotype: parseFloat(item.Phenotype),
     Haplotype: item.Haplotype,
     Line: item.Line,
   }))
-  const data: GenomeScanData = {
+  const data: Types.GenomeScanData = {
     lod_score_on_chromosome: genomeScanData.map(item => ({
       ...item,
       pos: parseInt(item.pos),
@@ -55,7 +57,7 @@ Promise.all([
       threshold: parseFloat(item.threshold)
     })),
   };
-  const plotCoefData: PlotCoefData[] = rawPlotCoefData.map(item => ({
+  const plotCoefData: Types.PlotCoefData[] = rawPlotCoefData.map(item => ({
     ...item,
     marker: item.marker,
     chr: item.chr,
@@ -72,14 +74,14 @@ Promise.all([
   }));
 
 
-  const snpsData: SnpsData[] = rawSnpsData.map(item => ({
+  const snpsData: Types.SnpsData[] = rawSnpsData.map(item => ({
     ...item,
     snp_id: item.snp_id,
     chr: item.chr,
     pos: parseFloat(item.pos),
     lod: parseFloat(item.lod)
   }));
-  const snpData: SnpData[] = rawSnpData.map(item => {
+  const snpData: Types.SnpData[] = rawSnpData.map(item => {
     return {
       ...item,
       line: item.line,
