bfc115c21c326e0cc6c42facc800ecc1a2ec15dd
Remi Planel <rplanel@pasteur.fr>
2021-07-29 13:21:04 +0200

    Transform dataCount to js Map

--- examples/bigsdb/main.ts
+++ examples/bigsdb/main.ts
@@ -1,7 +1,8 @@
 import { select } from "d3-selection";
 import { json } from "d3-fetch";
-import { IsolateCount, BigsdbMap } from "bioviz-js";
-import { Topology } from "topojson-specification";
+import { BigsdbMap } from "bioviz-js";
+import { Topology, Objects } from "topojson-specification";
+import { IsolateCount } from "../../lib/types";
 
 console.log("testttt")
 const BigsdbMapComponent = BigsdbMap()
@@ -9,19 +10,28 @@ const bigsdbMap = select("svg")
     .attr("width", 1280)
     .attr("height", 800);
 
+
 // const countryUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json"
 const countryUrl = "https://bigsdb.pasteur.fr/javascript/topojson/countries.json"
 const dataCountUrl = "https://bigsdb.pasteur.fr/cgi-bin/bigsdb/bigsdb.pl?db=pubmlst_klebsiella_isolates&page=plugin&name=FieldBreakdown&field=country"
 console.log(bigsdbMap)
-json<Array<IsolateCount>>(dataCountUrl).then(dataCount => {
-    json<Topology>(countryUrl).then((world) => {
-        console.log(dataCount)
-        const dataCountMap = new Map(dataCount.map(d => [d.label, d]))
-        console.log(dataCountMap)
-        console.log(world)
+json<IsolateCount[]>(dataCountUrl).then((dataCount) => {
+    json<Topology<Objects<GeoJSON.GeoJsonProperties>>>(countryUrl).then((world) => {
+        const dataCountMap: Map<string, IsolateCount> = new Map(dataCount.map(d => [d.label, d]))
+        console.log([...dataCountMap.keys()].sort())
+        sumIsolateValues(dataCountMap, "United States", "USA")
+        sumIsolateValues(dataCountMap, "Spain", "SPAIN")
+        sumIsolateValues(dataCountMap, "Brazil", "BRAZIL")
+        sumIsolateValues(dataCountMap, "China", "CHINA")
         bigsdbMap.datum(world).call(BigsdbMapComponent, window.innerWidth, dataCountMap);
 
     })
 })
 
 
+function sumIsolateValues(dataCountMap, countryName, nameSumWith) {
+    console.log(dataCountMap.get(nameSumWith))
+    console.log(dataCountMap.get(countryName))
+    const newValue = dataCountMap.get(countryName).value + dataCountMap.get(nameSumWith).value
+    dataCountMap.set(countryName, { ...dataCountMap.get(countryName), value: newValue })
+}
